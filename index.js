5202e05f00fa14ac1784f0097ec87df8994dc2cb
singhmuskan750 <singhmuskan0099@gmail.com>
2021-09-21 22:12:51 +0530

    js changes done

---
+++ index.js
@@ -0,0 +1,16 @@
+function openTab(event, tabName) {
+  
+    let tabcontent = document.getElementsByClassName("tabcontent");
+    for (let i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    } 
+  
+    let tablinks = document.getElementsByClassName("tablinks");
+    for (let i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    document.getElementById(tabName).style.display = "block";
+    event.currentTarget.className += " active";
+  }
+  
